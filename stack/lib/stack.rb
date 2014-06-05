cda9077bce8ce55d40868930419b72db1f6bd16b
marco-lindsay <lindsay_marco@hotmail.com>
2014-06-04 18:51:57 -0700

    initial stack commit

---
+++ stack/lib/stack.rb
@@ -0,0 +1,37 @@
+class Stack
+  attr_accessor :head, :count
+
+  def initialize
+    @count = 0
+  end
+
+  def push(value)
+    current_node = Node.new(value)
+    if @head.nil?
+      @head = current_node
+    else
+      current_node.next = @head
+      @head = current_node
+    end
+    @count += 1
+  end
+
+  def pop
+    if @count == 0
+      return nil
+    else
+      old_head = @head
+      @head.next_node = @head
+      return old_head.value
+    end
+    @count -= 1
+  end
+
+ class Node
+    attr_accessor :value, :next_node
+
+    def initialize(value)
+      @value = value
+      @next_node = nil
+    end
+  end

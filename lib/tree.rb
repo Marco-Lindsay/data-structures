759a5b75b79de793dd0e9b025fb09588dc64b5e0
marco-lindsay <lindsay_marco@hotmail.com>
2014-06-05 19:03:18 -0700

    add binary tree with functionality

---
+++ lib/tree.rb
@@ -0,0 +1,32 @@
+class Tree
+attr_accessor :value, :left, :right
+
+def initialize(value, left=nil, right=nil)
+  @value = value
+  @left = left
+  @right = right
+  @@output = ""
+end
+
+def pre_order
+  @@output += value + " "
+  left.pre_order if left
+  right.pre_order if right
+  @@output.chop
+end
+
+def post_order
+  left.post_order if left
+  right.post_order if right
+  @@output += value + " "
+  @@output.chop
+end
+
+def in_order
+  left.in_order if left
+  @@output += value + " "
+  right.in_order if right
+  @@output.chop
+end
+
+end

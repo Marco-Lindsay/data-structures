9f67e36da9f5d4eb63371aef73ac305f7adee243
marco-lindsay <lindsay_marco@hotmail.com>
2014-06-08 22:33:03 -0700

    put output to array instead of global variable

--- lib/tree.rb
+++ lib/tree.rb
@@ -5,28 +5,28 @@ def initialize(value, left=nil, right=nil)
   @value = value
   @left = left
   @right = right
-  @@output = ""
+  @@output = []
 end
 
 def pre_order
-  @@output += value + " "
+  @@output << value
   left.pre_order if left
   right.pre_order if right
-  @@output.chop
+  @@output
 end
 
 def post_order
   left.post_order if left
   right.post_order if right
-  @@output += value + " "
-  @@output.chop
+  @@output << value
+  @@output
 end
 
 def in_order
   left.in_order if left
-  @@output += value + " "
+  @@output << value
   right.in_order if right
-  @@output.chop
+  @@output
 end
 
 end
--- test/tree_test.rb
+++ test/tree_test.rb
@@ -17,15 +17,17 @@ describe "binary tree print methods"  do
   end
 
   it "will return a pre order search" do
-    @tree.pre_order.must_equal "Tim Jony Dan Katie Peter Andrea Phil Craig Eddie"
+    @tree.pre_order.must_equal ["Tim", "Jony", "Dan", "Katie", "Peter", "Andrea", "Phil", "Craig", "Eddie"]
   end
 
   it "will return a post order search" do
-    @tree.post_order.must_equal "Dan Peter Andrea Katie Jony Craig Eddie Phil Tim"
+    @tree.post_order.must_equal ["Dan", "Peter", "Andrea", "Katie", "Jony", "Craig", "Eddie", "Phil", "Tim"]
+
   end
 
   it "will return an in order search" do
-    @tree.in_order.must_equal "Dan Jony Peter Katie Andrea Tim Craig Phil Eddie"
+    @tree.in_order.must_equal ["Dan", "Jony", "Peter", "Katie", "Andrea", "Tim", "Craig", "Phil", "Eddie"]
+
   end
 
 end
